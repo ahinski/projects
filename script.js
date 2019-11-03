2ba502f76e5f2c324cf0e0913ec8b16f9f3ba283
Denis Ahinski <ahinski.denis@gmail.com>
2019-11-03 23:37:36 +0300

    feat: add slider and complete desktop version

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ educationBtn.addEventListener("click", () => {
         }
     }
 });
+
+let currentSibling = document.getElementsByClassName('projects-slider-item_container')[0].firstElementChild;
+console.log(currentSibling.nextElementSibling);
+console.log(currentSibling.nextElementSibling);
+
+controlLeft.addEventListener("click", () => {
+    if (currentSibling.previousElementSibling === null) {
+        console.log('A');
+        currentSibling.classList.remove("slider-current");
+        currentSibling = document.getElementsByClassName('projects-slider-item_container')[0].lastElementChild;
+        currentSibling.classList.add("slider-current");
+    }
+    else {
+        currentSibling.classList.remove("slider-current");
+        currentSibling = currentSibling.previousElementSibling;
+        currentSibling.classList.add("slider-current");
+    }
+});
+
+controlRight.addEventListener("click", () => {
+    if (currentSibling.nextElementSibling === null) {
+        currentSibling.classList.remove("slider-current");
+        currentSibling = document.getElementsByClassName('projects-slider-item_container')[0].firstElementChild;
+        currentSibling.classList.add("slider-current");
+    }
+    else {
+        currentSibling.classList.remove("slider-current");
+        currentSibling = currentSibling.nextElementSibling;
+        currentSibling.classList.add("slider-current");
+    }
+});
